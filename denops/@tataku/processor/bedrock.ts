4c3fccf412623711439ce493dc9c21890dd6a579
Omochice <mochice.mls.ntl@gmail.com>
2025-01-29 22:41:04 +0900

    feat: use aws-sdk instead

--- denops/@tataku/processor/bedrock.ts
+++ denops/@tataku/processor/bedrock.ts
@@ -1,12 +1,14 @@
-import "npm:@smithy/eventstream-codec@^2.0.5";
-
 import { Denops } from "jsr:@denops/std@7.4.0";
 import { assert, is } from "jsr:@core/unknownutil@4.3.0";
 import { toTransformStream } from "jsr:@std/streams@1.0.8/to-transform-stream";
-import { BedrockChat } from "npm:@langchain/community@0.2.21/chat_models/bedrock/web";
 import { fromIni } from "npm:@aws-sdk/credential-providers@3.734.0";
-import { HumanMessage } from "npm:@langchain/core@0.2.18/messages";
 import { ProcessorFactory } from "jsr:@omochice/tataku-vim@1.1.0";
+import {
+  BedrockRuntimeClient,
+  type ConverseCommandInput,
+  ConverseStreamCommand,
+  type Message,
+} from "npm:@aws-sdk/client-bedrock-runtime@3.734.0";
 
 const isOption = is.ObjectOf({
   model: is.String,
@@ -19,25 +21,33 @@ const processor: ProcessorFactory = async (
   option: unknown,
 ) => {
   assert(option, isOption);
-  const { accessKeyId, secretAccessKey, sessionToken } = await fromIni({
+  const credentials = await fromIni({
     profile: option.profile,
   })();
-  const model = new BedrockChat({
-    model: option.model,
+  const model = new BedrockRuntimeClient({
     region: option.region,
-    credentials: {
-      accessKeyId,
-      secretAccessKey,
-      sessionToken,
-    },
+    endpoint: `https://bedrock-runtime.${option.region}.amazonaws.com`,
+    credentials,
   });
   return toTransformStream(async function* (src: ReadableStream<string[]>) {
     for await (const chunk of src) {
-      const response = await model.stream([
-        new HumanMessage({ content: chunk.join("") }),
-      ]);
-      for await (const r of response) {
-        yield [r.content.toString()];
+      const messages = chunk.map((text, i): Message => ({
+        role: i % 2 === 0 ? "user" : "assistant",
+        content: [{ text }],
+      }));
+      const input: ConverseCommandInput = {
+        modelId: option.model,
+        messages,
+      };
+      const command = new ConverseStreamCommand(input);
+      const response = await model.send(command);
+
+      if (response.stream) {
+        for await (const chunk of response.stream) {
+          if (chunk.contentBlockDelta?.delta?.text) {
+            yield [chunk.contentBlockDelta.delta.text];
+          }
+        }
       }
     }
   });
